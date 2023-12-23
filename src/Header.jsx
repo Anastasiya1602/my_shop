15f8166a4ab64a1616b75441f1eb06fcbe4d8f8b
Anastasiia Chavdar <chavdaranastasiya85@yandex.ru>
2023-12-24 00:15:35 +0300

    замена лого на кнопку Liked  для дальнейшей фильтрации карточек по лайкам

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,9 @@
 import logo from './assets/img/logo.svg'
-// import basket from './assets/img/cart.svg'
 
 export const Header = () => {
     return (
       <div className='header'>
         <img src={logo} alt="logo" />
-        {/* <img src={basket} alt="logo" /> */}
         <button className='button_liked'>Liked</button>
 
       </div>
